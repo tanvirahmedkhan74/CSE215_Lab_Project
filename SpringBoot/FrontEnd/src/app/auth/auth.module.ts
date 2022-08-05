ea110f2dd57e0bdf1f748e2fb6f28d3e0633bc03
tanvirahmedkhan74 <tanvirahmedkhan74@gmail.com>
2022-08-05 13:11:16 +0600

    Registration Page and api Connect Successful!

---
+++ SpringBoot/FrontEnd/src/app/auth/auth.module.ts
@@ -0,0 +1,15 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { SignupComponent } from './signup/signup.component';
+// import { ReactiveFormsModule } from '@angular/forms';
+
+// @NgModule({
+//   declarations: [
+//     SignupComponent
+//   ],
+//   imports: [
+//     CommonModule,
+//     ReactiveFormsModule
+//   ]
+// })
+// export class AuthModule { }
